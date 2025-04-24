91de902c8d55745e2115dbabe8866d407d6abd77
Kamil Mukhtarov <boybutterfly1@gmail.com>
2025-04-24 05:35:02 +0300

    refactor

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,7 +5,7 @@
     class="scrolled-block relative z-20 mb-10 flex h-[calc(100dvh-4.5rem)] w-full flex-col rounded-t-xl border-b border-dotted border-b-c-white/30 bg-c-dark p-3"
   >
     <div class="grid h-full grid-cols-8 grid-rows-2 gap-2.5">
-      <img loading="lazy" src="/images/globe.webp" width="100" alt="logo" class="" />
+      <img loading="lazy" src="/images/globe.webp" width="100" alt="logo" class="aspect-square" />
       <div class="text-[0.65rem] text-c-white">2025®</div>
       <span class="col-start-5 col-end-7 row-start-1 row-end-2 text-[0.5rem] text-c-white">
         {{ $t('decoration.rights') }}
