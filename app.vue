71eedcd7ca18a79c309d198825405d4e2f4e960f
Kamil Mukhtarov <boybutterfly1@gmail.com>
2025-05-07 11:13:47 +0300

    fix

--- app.vue
+++ app.vue
@@ -7,15 +7,27 @@
     <NuxtPage />
   </NuxtLayout>
   <BottomNav class="fixed bottom-0 left-0 z-50" />
-  <Analytics />
+  <ClientOnly>
+    <Analytics v-if="isGuest" />
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt';
 import { useHead, useI18n } from '#imports';
-import { watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const { locale } = useI18n();
+
+const isGuest = ref<boolean>(false);
+
+const checkGuest = () => {
+  const flag: string | null = localStorage.getItem('isGuest');
+
+  if (flag) {
+    isGuest.value = JSON.parse(flag);
+  } else isGuest.value = true;
+};
 
 useHead({
   htmlAttrs: {
@@ -23,7 +35,7 @@ useHead({
   },
   meta: [{ name: 'description', content: 'My presentation website. Creator: @btrfly11.' }],
 });
-
+onMounted(() => checkGuest());
 watch(locale, (newLocale) => {
   useHead({
     htmlAttrs: {
